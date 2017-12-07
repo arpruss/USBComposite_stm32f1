bbf41fc2ee48245b8e24f25db20cbab3c64396cc
arpruss <arpruss@gmail.com>
2017-12-06 21:50:19 -0600

    demo

---
+++ examples/twojoysticks/twojoysticks.ino
@@ -0,0 +1,40 @@
+#include <USBHID.h>
+
+const uint8_t reportDescription[] = {
+   USB_HID_MOUSE_REPORT_DESCRIPTOR(USB_HID_MOUSE_REPORT_ID),
+   USB_HID_KEYBOARD_REPORT_DESCRIPTOR(USB_HID_KEYBOARD_REPORT_ID),
+   USB_HID_JOYSTICK_REPORT_DESCRIPTOR(USB_HID_JOYSTICK_REPORT_ID),
+   USB_HID_JOYSTICK_REPORT_DESCRIPTOR(USB_HID_JOYSTICK_REPORT_ID+1),
+};
+
+HIDJoystick Joystick2(USB_HID_JOYSTICK_REPORT_ID+1);
+
+void setup(){
+  HID.begin(reportDescription, sizeof(reportDescription));
+  Joystick.setManualReportMode(true);
+  Joystick2.setManualReportMode(true);
+}
+
+void loop(){
+  Joystick.X(0);
+  Joystick.Y(0);
+  Joystick.sliderRight(1023);
+  Joystick.sendManualReport();
+  delay(400);
+  Joystick.X(1023);
+  Joystick.Y(1023);
+  Joystick.sliderRight(0);
+  Joystick.sendManualReport();
+  delay(400);
+  Joystick2.X(0);
+  Joystick2.Y(0);
+  Joystick2.sliderRight(1023);
+  Joystick2.sendManualReport();
+  delay(400);
+  Joystick2.X(1023);
+  Joystick2.Y(1023);
+  Joystick2.sliderRight(0);
+  Joystick2.sendManualReport();
+  delay(400);
+}
+
