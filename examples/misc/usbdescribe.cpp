643f38c0ed0c4f285321590d9a55b02bb61beb0d
arpruss <arpruss@gmail.com>
2017-12-18 15:26:30 -0600

    foo

--- examples/misc/usbdescribe.cpp
+++ examples/misc/usbdescribe.cpp
@@ -18,7 +18,15 @@ template<unsigned interfaces, unsigned endpoints, uint8 interfaceStart, uint8 en
     constexpr static unsigned numInterfaces = interfaces;
     constexpr static unsigned numEndpoints = endpoints;
     constexpr static unsigned descriptor_config_size = (unsigned int)sizeof...(args);
-    constexpr static uint8 descriptor_config[] = { convertByte(args, interfaceStart, endpointStart)... };
+    constexpr uint8 convertByte(unsigned x) {
+        if (x&ENDPOINT_MASK) 
+            return (x&0xFF)+endpointStart;
+        else if (x&INTERFACE_MASK)
+            return (x&0xFF)+interfaceStart;
+        else
+            return x;
+    }
+    constexpr static uint8 descriptor_config[] = { convertByte(args)... };
 };
 
 constexpr USBCompositeComponent<1,2, 10,20, 1,2|ENDPOINT_MASK,3|INTERFACE_MASK> alpha;
