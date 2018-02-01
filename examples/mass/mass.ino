4f80bcd81f157e9f4b46245ac0dbd6ca6f8381b2
arpruss <arpruss@gmail.com>
2018-02-01 09:37:36 -0600

    foo

--- examples/mass/mass.ino
+++ examples/mass/mass.ino
@@ -1,68 +1,66 @@
 #include <MassStorage.h>
-#include <usb_mass_mal.h>
 #include <USBHID.h>
 
-//#define READ_ONLY
-
-#ifdef READ_ONLY
 #include "image.h"
-#define buffer image
-#else
-#define DISK_SIZE 12000 // sizeof(image) // 5120 // sizeof(image)
-uint8 buffer[DISK_SIZE];
-#endif
 
-uint32_t MAL_massBlockCount[2];
-uint32_t MAL_massBlockSize[2];
+bool write(uint32_t memoryOffset, const uint8_t *writebuff, uint16_t transferLength) {
+  memcpy(image+memoryOffset, writebuff, transferLength);
+
+  return true;
+}
+
+bool read(uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
+  memcpy(readbuff, image+memoryOffset, transferLength);
+  
+  return true;
+}
+
+char hexNibble(uint8 x) {
+  return x < 10 ? x + '0' : x + 'A' - 10;
+}
+
+char* format16(uint16 c) {
+  static char str[6];
+  str[5] = 0;
+  char *p = str+5;
+  do {
+    *--p = (c % 10) + '0';
+    c /= 10;
+  } while(c);
+  return p;
+}
+
+void dumpDrive() {
+  char hex[7] = "0x11,";
+  CompositeSerial.print("uint8 image[");
+  CompositeSerial.print(format16(sizeof(image)));
+  CompositeSerial.println("] = {");
+  int last;
+  for (last=sizeof(image)-1;last>=0 && image[last] == 0;last--);
+  if (last<0) last=0;
+  
+  for (int i=0; i<=last; i++) {
+    if (i && i % 16 == 0)
+      CompositeSerial.println("");
+    hex[2] = hexNibble(image[i]>>4);
+    hex[3] = hexNibble(image[i]&0xF);
+    CompositeSerial.print(hex);
+  }
+  CompositeSerial.println("\n};\n");
+}
 
 void setup() {
-  pinMode(PB12,OUTPUT);
-  digitalWrite(PB12,1);
-  MAL_massBlockCount[0] = DISK_SIZE / 512;
-  MAL_massBlockCount[1] = 0;
-  MAL_massBlockSize[0] = 512;
-  MAL_massBlockSize[1] = 0;
-  USBMassStorage.begin();
+  USBMassStorage.setDrive(0, sizeof(image), read, write);
+  USBComposite.add(USBMassStorage);
+  USBComposite.add(CompositeSerial);
+  USBComposite.begin();
   delay(2000);
- // CompositeSerial.println("hello");
 }
 
-extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) {
-  if (lun != 0) {
-    return USB_MASS_MAL_FAIL;
-  }
-#ifndef READ_ONLY  
-  memcpy(buffer+memoryOffset, writebuff, transferLength);
-#endif  
-  CompositeSerial.println("write");
-  return USB_MASS_MAL_SUCCESS;
-}
-
-extern "C" uint16_t usb_mass_mal_init(uint8_t lun) {
-  return USB_MASS_MAL_SUCCESS;
-}
-
-extern "C" uint16_t usb_mass_mal_get_status(uint8_t lun) {  
-  return USB_MASS_MAL_SUCCESS;
-}
-
-extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
-  if (lun != 0) {
-    return USB_MASS_MAL_FAIL;
-  }
-  CompositeSerial.println(String(memoryOffset)+" "+String(transferLength));
-  memcpy(readbuff, buffer+memoryOffset, transferLength);
-  return USB_MASS_MAL_SUCCESS;
-}
-
-extern "C" void usb_mass_mal_format() {
-}
-
-int state = 0;
 void loop() {
-  digitalWrite(PB12,state);
-//  delay(20);
-  state = !state;
   USBMassStorage.loop();
+  if (CompositeSerial.available() && 'd' == CompositeSerial.read()) {
+      dumpDrive();
+  }
 }
 
