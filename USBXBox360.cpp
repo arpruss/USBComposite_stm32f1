ed35e7b171c4ab362c3128fd963a13d2d4850074
arpruss <arpruss@gmail.com>
2018-06-25 23:17:43 -0500

    foo

--- USBXBox360.cpp
+++ USBXBox360.cpp
@@ -45,6 +45,8 @@ void USBXBox360::setLEDCallback(void (*callback)(uint8 pattern)) {
 
 bool USBXBox360::init(void* ignore) {
 	(void)ignore;
+    USBComposite.setVendorId(0x045e);
+    USBComposite.setProductId(0x028e);
 	usb_generic_set_info(0x045e, 0x028e, NULL, NULL, NULL);
 	return true;
 }
