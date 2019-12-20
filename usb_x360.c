12955104f96a5a735cc31693720d6da3856298dd
arpruss <arpruss@gmail.com>
2019-12-20 17:06:48 -0600

    allow x360 endpoint number change

--- usb_x360.c
+++ usb_x360.c
@@ -247,7 +247,7 @@ static const usb_descriptor_config X360Descriptor_Config =
 	},
     
     .unknown_descriptor1 = {
-        17,33,0,1,1,37,129,20,0,0,0,0,19,2,8,0,0,
+        17,33,0,1,1,37,129,20,0,0,0,0,19,2/*PATCH*/,8,0,0,
     }, 
 	
 	.DataInEndpoint = {
@@ -292,6 +292,7 @@ static void getX360PartDescriptor(uint8* out) {
     OUT_BYTE(X360Descriptor_Config, HID_Interface.bInterfaceNumber) += usbX360Part.startInterface;
     OUT_BYTE(X360Descriptor_Config, DataOutEndpoint.bEndpointAddress) += usbX360Part.startEndpoint;
     OUT_BYTE(X360Descriptor_Config, DataInEndpoint.bEndpointAddress) += usbX360Part.startEndpoint;
+    OUT_BYTE(X360Descriptor_Config, unknown_descriptor1[13]) = USB_X360_RX_ENDP;
 }
 
 USBCompositePart usbX360Part = {
